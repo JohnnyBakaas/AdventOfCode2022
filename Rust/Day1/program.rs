536efcbefbd5a194a934a230ecec051a268615f8
JohnnyBakaas <johnny@bakaas-cadconsult.no>
2023-08-01 17:48:06 +0200

    Day1 part2 Rust btw

--- Rust/Day1/program.rs
+++ Rust/Day1/program.rs
@@ -8,15 +8,17 @@ How to run:
 use std::fs;
 
 fn main() {
+    part_two()
+}
+
+fn part_one() {
     let lines = get_list();
     let mut most_largest: u32 = 0;
     let mut temp: u32 = 0;
 
     for line in &lines {
         if line.len() > 1 {
-            let numb: u32 = clac_total_part(line.to_string());
-            println!("part {}", line);
-            print_type_of(&numb);
+            let numb: u32 = string_to_u32(line.to_string());
             temp += numb;
         } else {
             if most_largest < temp {
@@ -30,6 +32,39 @@ fn main() {
     println!("most_largest {}", most_largest);
 }
 
+fn part_two() {
+    let lines = get_list();
+    let mut most_largest: u32 = 0;
+    let mut second_most_largest: u32 = 0;
+    let mut third_most_largest: u32 = 0;
+    let mut temp: u32 = 0;
+
+    for line in &lines {
+        if line.len() > 1 {
+            let numb: u32 = string_to_u32(line.to_string());
+            temp += numb;
+        } else {
+            if most_largest < temp {
+                third_most_largest = second_most_largest;
+                second_most_largest = most_largest;
+                most_largest = temp;
+            } else if second_most_largest < temp {
+                third_most_largest = second_most_largest;
+                second_most_largest = temp;
+            } else if third_most_largest < temp {
+                third_most_largest = temp;
+            }
+
+            temp = 0
+        }
+    }
+
+    println!(
+        "most_total_largest {}",
+        most_largest + second_most_largest + third_most_largest
+    );
+}
+
 fn get_list() -> Vec<String> {
     let file_path = "C:/Users/johnn/OneDrive/Dokumenter/GitHub/cv-generic-react/AdventOfCode2022/Rust/Day1/adventofcode.com_2022_day_1_input.txt";
     println!("In file {}", file_path);
@@ -41,7 +76,7 @@ fn get_list() -> Vec<String> {
     return lines;
 }
 
-fn clac_total_part(inp: String) -> u32 {
+fn string_to_u32(inp: String) -> u32 {
     let num: u32 = inp.parse().unwrap();
     return num;
 }
